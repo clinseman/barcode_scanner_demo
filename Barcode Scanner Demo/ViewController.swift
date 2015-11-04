b641f7cda080a67fcc23efb87534d1927dbc2372
Chris <clinseman@gmail.com>
2015-11-04 10:05:45 +0000

    Update to project settings and Swift language

--- Barcode Scanner Demo/ViewController.swift
+++ Barcode Scanner Demo/ViewController.swift
@@ -12,10 +12,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         super.viewDidLoad()
         
         // Allow the view to resize freely
-        self.highlightView.autoresizingMask =   UIViewAutoresizing.FlexibleTopMargin |
-            UIViewAutoresizing.FlexibleBottomMargin |
-            UIViewAutoresizing.FlexibleLeftMargin |
-            UIViewAutoresizing.FlexibleRightMargin
+        self.highlightView.autoresizingMask =   [UIViewAutoresizing.FlexibleTopMargin, UIViewAutoresizing.FlexibleBottomMargin, UIViewAutoresizing.FlexibleLeftMargin, UIViewAutoresizing.FlexibleRightMargin]
         
         // Select the color you want for the completed scan reticle
         self.highlightView.layer.borderColor = UIColor.greenColor().CGColor
@@ -30,10 +27,9 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         
         // Create a nilable NSError to hand off to the next method.
         // Make sure to use the "var" keyword and not "let"
-        var error : NSError? = nil
-        
-        
-        let input : AVCaptureDeviceInput? = AVCaptureDeviceInput.deviceInputWithDevice(device, error: &error) as? AVCaptureDeviceInput
+        let error : NSError? = nil
+
+        let input : AVCaptureDeviceInput? = try! AVCaptureDeviceInput(device: device)
         
         // If our input is not nil then add it to the session, otherwise we're kind of done!
         if input != nil {
@@ -41,7 +37,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         }
         else {
             // This is fine for a demo, do something real with this in your app. :)
-            println(error)
+            print(error)
         }
         
         let output = AVCaptureMetadataOutput()
@@ -50,7 +46,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         output.metadataObjectTypes = output.availableMetadataObjectTypes
         
         
-        previewLayer = AVCaptureVideoPreviewLayer.layerWithSession(session) as AVCaptureVideoPreviewLayer
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
         previewLayer.frame = self.view.bounds
         previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
         self.view.layer.addSublayer(previewLayer)
@@ -88,11 +84,11 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
             for barcodeType in barCodeTypes {
                 
                 if metadata.type == barcodeType {
-                    barCodeObject = self.previewLayer.transformedMetadataObjectForMetadataObject(metadata as AVMetadataMachineReadableCodeObject)
+                    barCodeObject = self.previewLayer.transformedMetadataObjectForMetadataObject(metadata as! AVMetadataMachineReadableCodeObject)
                     
                     highlightViewRect = barCodeObject.bounds
                     
-                    detectionString = (metadata as AVMetadataMachineReadableCodeObject).stringValue
+                    detectionString = (metadata as! AVMetadataMachineReadableCodeObject).stringValue
                     
                     self.session.stopRunning()
                     break
@@ -101,7 +97,7 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
             }
         }
         
-        println(detectionString)
+        print(detectionString)
         self.highlightView.frame = highlightViewRect
         self.view.bringSubviewToFront(self.highlightView)
         
